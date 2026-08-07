91026beaaeef9c8c1bd080d2ef407f3c01a69732
Filip Tomin <tominf6453@gmail.com>
2026-08-07 12:48:59 -0400

    making a data based card grid, anticipating a 2D grid with a status element to find the closest vailable slot for placing cards, just constructing the grid for now

--- CardGameTakeHomeStarter/Assets/Scripts/PlayAreaManager.cs
+++ CardGameTakeHomeStarter/Assets/Scripts/PlayAreaManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Linq;
 
 namespace CardGame {
     /// <summary>
@@ -27,9 +28,14 @@ namespace CardGame {
 		#endregion
 
 		#region Local Vars
+		private CardSlot[,] cardSlotGrid;
 		#endregion
 
 		#region Parameters/Getters
+		public static int GridXSize => Instance.playAreaWidthCards;
+		public static int GridYSize => Instance.playAreaHeightCards;
+		public static float LateralCardOffset => Instance.lateralCardOffset;
+		public static float VerticalCardOffset => Instance.verticalCardOffset;
 		#endregion
 
 		#region Mono Implementation
@@ -38,16 +44,67 @@ namespace CardGame {
 			if ( Instance == null ) Destroy( this );
 			else Instance = this;
 		}
+
+		private void Start() {
+			// Generate the card grid starting with the one in the middle.
+			ConstructCardGrid();
+		}
 		#endregion
 
 		#region Events
 		#endregion
 
 		#region Helpers
+		private void ConstructCardGrid() {
+			// Define a 2D array to store all the card slots.
+			cardSlotGrid = new CardSlot[playAreaWidthCards , playAreaHeightCards];
+
+			// Instantiate all the slots.
+			for ( int x = 0; x < playAreaWidthCards; x++ ) { 
+				for ( int y = 0; y < playAreaHeightCards; y++ ) {
+					float canvasX = lateralCardOffset * (x - playAreaWidthCards / 2 ); // Int division should floor.
+					float canvasY = verticalCardOffset * (y - playAreaHeightCards / 2 ); // Means edgemost pos will be 0 - width/height, or -width/height
+
+					// Instantiate slot.
+					cardSlotGrid[x , y] = new( canvasX , canvasY , x , y );
+
+					// Check if slot is middle, set to available.
+					if ( canvasX == 0 && canvasY == 0 ) {
+						cardSlotGrid[x , y].status = CardSlot.CardSlotStatus.Available;
+					}
+				}
+			}
+		}
 		#endregion
 
 		#region Coroutines
 		#endregion
 
+	}
+
+	public class CardSlot {
+		public enum CardSlotStatus { Locked, Available, Full }
+
+		float canvasXPos, canvasYPos;
+		int gridXPos, gridYPos;
+
+		public CardSlotStatus status = CardSlotStatus.Locked;
+
+		CardData presentCard;
+
+		public CardSlot( float canvasX, float canvasY, int gridX, int gridY ) {
+			canvasXPos = canvasX;
+			canvasYPos = canvasY;
+			gridXPos = gridX;
+			gridYPos = gridY;
+		}
+
+		public Vector2 GetCanvasPosition => new(canvasXPos, canvasYPos);
+		public (int,int) GetGridPosition => new(gridXPos, gridYPos); // Vector2 stores floats, so use a tuple instead.
+
+		public void SetCard( CardData card ) {
+			presentCard = card;
+			status = CardSlotStatus.Full;
+		}
 	}
 }
